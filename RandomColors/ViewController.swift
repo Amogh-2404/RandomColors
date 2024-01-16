178636231bdbd8a724d2a0d6de504d671cf8a189
Amogh-2404 <ramogh2404@gmail.com>
2024-01-16 18:55:25 +0530

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by R.AMOGH on 16/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
